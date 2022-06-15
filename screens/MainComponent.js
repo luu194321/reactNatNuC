addee8dd55a5676791d03fa7b5782bc0d608b9b7
Alex Luu <alex__luu@hotmail.com>
2022-06-15 17:22:12 -0500

    Navigation part 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,28 @@ import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
 import Constants from "expo-constants";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./HomeScreen";
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+  headerTintColor: "#fff",
+  headerStyle: { backgroundColor: "#5637DD" },
+};
+
+const HomeNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: "Home" }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
@@ -13,10 +35,7 @@ const DirectoryNavigator = () => {
     <Stack.Navigator
       initialRouteName="Directory"
       // default route this navigator uses when first loading
-      screenOptions={{
-        headerStyle: { backgroundColor: "#5637DD" },
-        headerTintColor: "#fff",
-      }}
+      screenOptions={screenOptions}
       // define the look and feel of nav header
     >
       <Stack.Screen
@@ -45,7 +64,21 @@ const Main = () => {
         paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
       }}
     >
-      <DirectoryNavigator />
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerStyle={{ backgroundColor: "#CEC8FF" }}
+      >
+        <Drawer.Screen
+          name="Home"
+          component={HomeNavigator}
+          options={{ title: "Home" }}
+        />
+        <Drawer.Screen
+          name="Directory"
+          component={DirectoryNavigator}
+          options={{ title: "Directory" }}
+        />
+      </Drawer.Navigator>
     </View>
   );
 };
