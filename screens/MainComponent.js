78dc4c206e1bd6f8ac4e8d79de0cc5471ae4d5e3
Alex Luu <alex__luu@hotmail.com>
2022-07-09 21:30:54 -0500

    wk4 ass

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -245,21 +245,24 @@ const Main = () => {
     dispatch(fetchComments());
   }, [dispatch]);
 
-  useEffect(() => {
-    NetInfo.fetch().then((connectionInfo) => {
-      Platform.OS === "ios"
-        ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
-        : ToastAndroid.show(
-            "Initial Network Connectivity Type: " + connectionInfo.type,
-            ToastAndroid.LONG
-          );
-    });
+  const showNetInfo = async () => {
+    const connectionInfo = await NetInfo.fetch();
+    Platform.OS === "ios"
+      ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
+      : ToastAndroid.show(
+          "Initial Network Connectivity Type: " + connectionInfo.type,
+          ToastAndroid.LONG
+        );
 
     const unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
       handleConnectivityChange(connectionInfo);
     });
 
     return unsubscribeNetInfo;
+  };
+
+  useEffect(() => {
+    showNetInfo();
   }, []);
 
   const handleConnectivityChange = (connectionInfo) => {
